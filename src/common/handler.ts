57b838e0a9c77ad8d3d660dc62af40277693eeb0
Roman Klepov <rklepov@gmail.com>
2021-12-13 00:47:48 +0300

    feat: add types to utility functions
    The first attempt, not all ts compiler type errors are resolved so far.

--- src/common/handler.ts
+++ src/common/handler.ts
@@ -1,6 +1,12 @@
-// handler.js
+// handler.ts
 
-export function defineHandler(router, f) {
+import BoardRouter from '../resources/boards/board.router';
+import TaskRouter from '../resources/tasks/task.router';
+import UserRouter from '../resources/users/user.router';
+
+type Router = UserRouter | BoardRouter | TaskRouter;
+
+export function defineHandler(router: Router, f) {
   return async (q, p) => {
     const { status, payload } = await router.service[f](q);
     p.code(status).send(payload);
--- src/common/reply.ts
+++ src/common/reply.ts
@@ -1,6 +1,19 @@
-// reply.js
+// reply.ts
 
-export function Reply(status, payload) {
+import { UserId, BoardId, TaskId } from '../db/database';
+import Board from '../resources/boards/board.model';
+import Task from '../resources/tasks/task.model';
+import User from '../resources/users/user.model';
+
+type Payload =
+  | User
+  | Board
+  | Task
+  | { userId: UserId }
+  | { boardId: BoardId }
+  | { taskId: TaskId };
+
+export function reply(status: number, payload?: Payload) {
   return { status, payload };
 }
 
