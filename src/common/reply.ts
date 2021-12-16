236926d5f146ad7234a97b3d3e7296242f57ede0
Roman Klepov <rklepov@gmail.com>
2021-12-16 11:42:27 +0300

    refactor: make reply utility func uniform
    Transformed it to a generic function.

--- src/common/reply.ts
+++ src/common/reply.ts
@@ -1,19 +1,6 @@
 // reply.ts
 
-import { UserId, BoardId, TaskId } from '../db/database';
-import Board from '../resources/boards/board.model';
-import Task from '../resources/tasks/task.model';
-import User from '../resources/users/user.model';
-
-type Payload =
-  | User
-  | Board
-  | Task
-  | { userId: UserId }
-  | { boardId: BoardId }
-  | { taskId: TaskId };
-
-export function reply(status: number, payload?: Payload) {
+export function reply<PayloadT>(status: number, payload?: PayloadT) {
   return { status, payload };
 }
 
