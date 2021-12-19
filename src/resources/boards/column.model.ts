7aa04e64e88e944478be219480144fe82c3d413f
Roman Klepov <rklepov@gmail.com>
2021-12-19 23:07:22 +0300

    docs: write tsdoc comments for column component

--- src/resources/boards/column.model.ts
+++ src/resources/boards/column.model.ts
@@ -7,19 +7,42 @@ import { genId } from '../../common/utils';
 import { BoardId, IBoardId } from './board.types';
 import { IColumnId, IColumn, ColumnId } from './column.types';
 
-// TODO: so far columns are not exposed as individual entities
-//       (in fact this is not so easy to achieve because columns are nested
-//        to a board and at the same the columns needs to be assigned an Id).
+/**
+ * Models the Column object which holds the unique **Id** along with the fields
+ * describing a column.
+ *
+ * @privateremarks
+ * TODO: so far columns are not exposed as individual entities (in fact this is
+ * not so easy to achieve because columns are nested to a board and at the same
+ * the columns needs to be assigned an Id).
+ */
 class Column implements IColumnId, IBoardId, IColumn {
   // ? wonder if the class fields can be somehow inferred from the JSON schema below ?
+  /**
+   * The unique column **Id**.
+   */
   columnId: ColumnId = genId(); // TODO: preferably should be private
 
+  /**
+   * The title of the column.
+   */
   title = '';
 
+  /**
+   * The order of the column.
+   */
   order = NaN;
 
+  /**
+   * The **Id** of the board to which this column belongs.
+   */
   boardId: BoardId = ''; // TODO: the id shouldn't be an empty string
 
+  /**
+   * The {@link Column} object constructor.
+   *
+   * @param Column - An object with the fields matching {@link Column.schema}.
+   */
   constructor(column: IColumn) {
     Object.assign(
       this,
@@ -27,24 +50,52 @@ class Column implements IColumnId, IBoardId, IColumn {
     );
   }
 
-  // TODO: setter needed because the request can contain the column Id
+  //
+  /**
+   * Sets the object **Id**.
+   * @privateremarks
+   * TODO: setter needed because the request can contain the column Id
+   */
   set id(columnId: ColumnId) {
     this.columnId = columnId;
   }
 
+  /**
+   * Returns the object **Id**.
+   */
   get id() {
     return this.columnId;
   }
 
+  /**
+   * Assigns the column to the board.
+   *
+   * @param boardId - The **Id** of the {@link Board} to which the column is
+   * assigned to.
+   *
+   * @returns `this` {@link Column} object.
+   */
   assignToBoard(boardId: BoardId) {
     Object.assign(this, { boardId });
     return this;
   }
 
+  /**
+   * Returns DTO equivalent of the **Column** object. Eventually used as the
+   * JSON body of HTTP response.
+   *
+   * @returns The DTO equivalent of the **Column** object.
+   */
   toJSON() {
     return pick(this, Object.keys(Column.schema.response.properties));
   }
 
+  /**
+   * The JSON schema for the column object validation.
+   *
+   * @privateremarks
+   * TODO: learn more about how to automatically generate this from OpenAPI spec
+   */
   static schema = {
     request: {
       type: 'object',
--- src/resources/boards/column.types.ts
+++ src/resources/boards/column.types.ts
@@ -2,12 +2,22 @@
 
 import { genId } from '../../common/utils';
 
+/**
+ * The unique **Id** of the column.
+ */
 export type ColumnId = ReturnType<typeof genId>;
 
+/**
+ * An abstraction of the object having **columnId** property (request message
+ * and {@link Column} class)
+ */
 export interface IColumnId {
   columnId: ColumnId;
 }
 
+/**
+ * An abstraction of the **Column** object.
+ */
 export interface IColumn {
   title: string;
   order: number;
