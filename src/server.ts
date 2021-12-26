76d84cbf12b8db72c25b37b3274fcc54d53e2209
Roman Klepov <rklepov@gmail.com>
2021-12-26 18:42:33 +0300

    feat: enable unhandled exceptions handling
    Set up the handlers for uncaughtException and unhandledRejection events,
    log the errors and exit the process with non-zero status code
    (indirectly, after stopping the server).

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,18 @@ import Logger from './common/logger';
 
 import App from './app';
 
+function stop(log: Logger, app: App) {
+  app
+    .stop()
+    .then(() => {
+      log.info('The server has been stopped');
+    })
+    .catch((e) => {
+      const errMsg = e instanceof Error ? e.message : String(e);
+      log.fatal(`Failed to stop the server: ${errMsg}`);
+    });
+}
+
 /**
  * The main entry point of the REST server application.
  * Creates the application instance and starts the server on the specified port.
@@ -14,17 +26,45 @@ import App from './app';
  * @param port - The server port number
  */
 function run(port: number, logLevel: LogLevel, logDir: string) {
-  const logger = new Logger(logLevel, logDir);
-  const app = new App(logger);
+  const log = new Logger(logLevel, logDir);
+  const app = new App(log);
+
   app
     .start(port)
     .then(() => {
-      logger.info('The server has been started successfully');
+      log.info('The server has been started successfully');
     })
     .catch((e) => {
       const errMsg = e instanceof Error ? e.message : String(e);
-      logger.fatal(`Failed to start the server on ${port}: ${errMsg}`);
+      log.fatal(`Failed to start the server on ${port}: ${errMsg}`);
+      process.exitCode = 1;
     });
+
+  process.on('uncaughtException', (error, origin) => {
+    log.pinoLogger.error(error);
+    log.pinoLogger.fatal(
+      { origin, error: error.toString() },
+      'Uncaught Exception'
+    );
+    stop(log, app);
+    process.exitCode = 127;
+    // don't want to exit here, need to stop the server first, the process will
+    // exit automatically after that
+    // process.exit();
+  });
+
+  process.on('unhandledRejection', (reason, promise) => {
+    log.pinoLogger.error(reason);
+    log.pinoLogger.fatal(
+      { reason: String(reason), promise: String(promise) },
+      'Unhandled Rejection'
+    );
+    stop(log, app);
+    process.exitCode = 126;
+    // don't want to exit here, need to stop the server first, the process will
+    // exit automatically after that
+    // process.exit();
+  });
 }
 
 run(
@@ -32,5 +72,15 @@ run(
   (LOG_LEVEL as LogLevel) || 'info',
   LOG_DIR || './logs'
 );
+
+// To check the unhandled exception handling uncomment the following line and
+// try to start the server (increase the timeout if necessary)
+//
+// setTimeout(() => { throw Error('Oops! unhandled exception'); }, 1000);
+
+// To check the unhandled rejection handling uncomment the following line and
+// try to start the server (increase the timeout if necessary)
+//
+// setTimeout(() => { Promise.reject(Error('Oops! unhandled rejection')); }, 1000);
 
 // __EOF__
