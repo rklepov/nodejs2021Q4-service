7136d3141739a71ebf387c0db9262732fc4fa110
Roman Klepov <rklepov@gmail.com>
2022-01-31 16:36:35 +0300

    fix: correct board migration script

--- src/db/migrations/1643566027790-CreateTask.ts
+++ src/db/migrations/1643566027790-CreateTask.ts
@@ -1,10 +1,12 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+// npm run db:migration:gen CreateTask
+
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CreateTask1643566027790 implements MigrationInterface {
-    name = 'CreateTask1643566027790'
+  name = 'CreateTask1643566027790';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
             CREATE TABLE "task" (
                 "taskId" uuid NOT NULL DEFAULT uuid_generate_v4(),
                 "title" character varying NOT NULL,
@@ -16,20 +18,22 @@ export class CreateTask1643566027790 implements MigrationInterface {
                 CONSTRAINT "PK_c5a68aa4b5c8d38a06f8e8d4c57" PRIMARY KEY ("taskId")
             )
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "task"
             ADD CONSTRAINT "FK_f316d3fe53497d4d8a2957db8b9" FOREIGN KEY ("userId") REFERENCES "user"("userId") ON DELETE
             SET NULL ON UPDATE NO ACTION
         `);
-    }
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
             ALTER TABLE "task" DROP CONSTRAINT "FK_f316d3fe53497d4d8a2957db8b9"
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE "task"
         `);
-    }
-
+  }
 }
+
+// __EOF__
+// __EOF__
--- src/db/migrations/1643636133111-CreateBoard.ts
+++ src/db/migrations/1643636133111-CreateBoard.ts
@@ -1,45 +1,57 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+// npm run db:migration:gen CreateBoard
 
-export class CreateBoard1643608415648 implements MigrationInterface {
-    name = 'CreateBoard1643608415648'
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+export class CreateBoard1643636133111 implements MigrationInterface {
+  name = 'CreateBoard1643636133111';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
             CREATE TABLE "board" (
                 "boardId" uuid NOT NULL DEFAULT uuid_generate_v4(),
                 "title" character varying NOT NULL,
                 CONSTRAINT "PK_8db634dbfdd38c51ac9aeed3b2a" PRIMARY KEY ("boardId")
             )
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
+            ALTER TABLE "board_column"
+            ALTER COLUMN "boardId"
+            SET NOT NULL
+        `);
+    await queryRunner.query(`
             ALTER TABLE "task"
             ALTER COLUMN "boardId"
             SET NOT NULL
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "board_column"
             ADD CONSTRAINT "FK_7d6b58efcc37a760ffd108eec72" FOREIGN KEY ("boardId") REFERENCES "board"("boardId") ON DELETE CASCADE ON UPDATE NO ACTION
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "task"
             ADD CONSTRAINT "FK_d88edac9d7990145ff6831a7bb3" FOREIGN KEY ("boardId") REFERENCES "board"("boardId") ON DELETE CASCADE ON UPDATE NO ACTION
         `);
-    }
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
             ALTER TABLE "task" DROP CONSTRAINT "FK_d88edac9d7990145ff6831a7bb3"
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "board_column" DROP CONSTRAINT "FK_7d6b58efcc37a760ffd108eec72"
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "task"
             ALTER COLUMN "boardId" DROP NOT NULL
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
+            ALTER TABLE "board_column"
+            ALTER COLUMN "boardId" DROP NOT NULL
+        `);
+    await queryRunner.query(`
             DROP TABLE "board"
         `);
-    }
-
+  }
 }
+
+// __EOF__
