048b9b307066a0e8426a645c2c03bc8ed1d193c3
Roman Klepov <rklepov@gmail.com>
2022-02-05 09:58:09 +0300

    feat: check AUTH_MODE in jwt guard
    JWT check now happens if we explicitly were told to work in auth mode
    with AUTH_MODE environment variable.

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,9 +1,21 @@
 // jwt-auth.guard.ts
 
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private readonly config: ConfigService) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    if (this.config.get('AUTH_MODE', 'true') === 'true') {
+      return super.canActivate(context);
+    }
+    return true;
+  }
+}
 
 // __EOF__
